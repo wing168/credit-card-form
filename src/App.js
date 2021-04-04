5f3b885394a18986d461bd6d27d3b6fe65a19ff1
wing168 <wh.man@btinternet.com>
2021-04-04 16:01:07 +0100

    added form with no styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [cardNum, setCardNum] = useState('');
+  const [cardName, setCardName] = useState('');
+  const [month, setMonth] = useState('');
+  const [year, setCardYear] = useState('');
+  const [cvv, setCvv] = useState('');
+
+  const handleChangeCardNum = (e) => {
+    
+    let value = e.target.value.replace(/\D/g, ''); //Removes whitespace before filtering into the setCardNum function
+
+    setCardNum(value.replace(/(\d{4})/, '$1 ').replace(/(\d{4}) (\d{4})/, '$1 $2 ').replace(/(\d{4}) (\d{4}) (\d{4})/, '$1 $2 $3 ')); //Max 16 digits with 3 spaces
+
+  }
+
+
+  console.log(cardNum);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App container-fluid'>
+      <h1>Credit card form</h1>
+      <form className='login-form'>
+        <input className='card-num form-control' name='cardNum' type='tel' maxLength="19" value={cardNum} placeholder='Your 16 digit card number' onChange={handleChangeCardNum} required />
+        <input className='card-name form-control' name='cardName' type='text' value={cardName} placeholder='Your name as it appears on the card' required />
+        <input className='end-date-month form-control' name='month' type='text' value={month} placeholder='Month' required />
+        <input className='end-date-year form-control' name='year' type='year' value={year} placeholder='Year' required />
+        <input className='cvv form-control' name='cvv' type='number' value={cvv} placeholder='CVV' required />
+      </form>
     </div>
   );
 }

212f4b5b9f85f87ec1c528cde70be8061ae97990
wing168 <wh.man@btinternet.com>
2021-04-13 14:58:17 +0100

    initial format of form and card template

--- src/App.js
+++ src/App.js
@@ -5,31 +5,59 @@ const App = () => {
 
   const [cardNum, setCardNum] = useState('');
   const [cardName, setCardName] = useState('');
-  const [month, setMonth] = useState('');
-  const [year, setCardYear] = useState('');
+  const [expiryDate, setExpiryDate] = useState('');
   const [cvv, setCvv] = useState('');
 
-  const handleChangeCardNum = (e) => {
-    
-    let value = e.target.value.replace(/\D/g, ''); //Removes whitespace before filtering into the setCardNum function
-
-    setCardNum(value.replace(/(\d{4})/, '$1 ').replace(/(\d{4}) (\d{4})/, '$1 $2 ').replace(/(\d{4}) (\d{4}) (\d{4})/, '$1 $2 $3 ')); //Max 16 digits with 3 spaces
-
+  const setStateFuncMapping = {
+    cardNum: setCardNum,
+    cardName: setCardName,
+    expiryDate: setExpiryDate,
+    cvv: setCvv
   }
 
 
-  console.log(cardNum);
+  const handleChange = (e) => {
+
+    const targetName = e.target.name;
+
+    if (targetName === "cardNum") {
+      let value = e.target.value.replace(/\D/g, ''); //Removes whitespace before filtering into the setCardNum function
+
+      setStateFuncMapping[targetName](value.replace(/(\d{4})/, '$1 ').replace(/(\d{4}) (\d{4})/, '$1 $2 ').replace(/(\d{4}) (\d{4}) (\d{4})/, '$1 $2 $3 ')); //Max 16 digits with 3 spaces
+ 
+      //handling the delete event when it reaches the space inserted above after every 4 digits
+      if (e.nativeEvent.inputType === "deleteContentBackward" && cardNum.charAt(cardNum.length - 1) === " ") {
+        setStateFuncMapping[targetName](cardNum.slice(0, -1));
+      }
+
+    } else {
+        setStateFuncMapping[targetName](e.target.value);
+    }
+  }
 
   return (
     <div className='App container-fluid'>
-      <h1>Credit card form</h1>
-      <form className='login-form'>
-        <input className='card-num form-control' name='cardNum' type='tel' maxLength="19" value={cardNum} placeholder='Your 16 digit card number' onChange={handleChangeCardNum} required />
-        <input className='card-name form-control' name='cardName' type='text' value={cardName} placeholder='Your name as it appears on the card' required />
-        <input className='end-date-month form-control' name='month' type='text' value={month} placeholder='Month' required />
-        <input className='end-date-year form-control' name='year' type='year' value={year} placeholder='Year' required />
-        <input className='cvv form-control' name='cvv' type='number' value={cvv} placeholder='CVV' required />
-      </form>
+      <div className="form-wrapper">
+        <form className='login-form'>
+          <label className="form-label">Card Number</label>
+          <input className='card-num form-control' name='cardNum' type='tel' maxLength="19" value={cardNum} placeholder='Your 16 digit card number' onChange={handleChange} required />
+          <label className="form-label">Card Name</label>
+          <input className='card-name form-control' name='cardName' type='text' value={cardName} placeholder='Your name as it appears on the card' onChange={handleChange} required />
+          <div className="row">
+            <div className="col-xl">
+              <label className="form-label">Expiration date</label>
+              <input className='end-date-month form-control' name='expiryDate' type='month' value={expiryDate}  onChange={handleChange} required />
+            </div>
+            <div className="col-xl">
+              <label className="form-label">CVV</label>
+              <input className='cvv form-control' name='cvv' type='tel' maxLength="4" value={cvv} placeholder='CVV' onChange={handleChange} required />
+            </div>
+           
+          </div>
+         
+        </form>
+      </div>
+      
     </div>
   );
 }
---
+++ src/components/card-template/card-template.component.jsx
@@ -0,0 +1,10 @@
+
+const CardTemplate = ({ cardNum, cardName, expiryDate, cvv }) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CardTemplate;
